324dd405db627251ae86bc75c426ef464e216305
MIHIR CHAVAN <mihirc0111@gmail.com>
2023-05-08 00:47:02 +0530

    New repo with all teams

---
+++ README.md
@@ -0,0 +1,19 @@
+# Ipl-win-probability-predictor
+A machine learning project to find out the win probability of the chasing team in an IPL match. Made specially for 2023 IPL season teams using kaggle dataset from 2008-2019. This is made as a part of ML project of my college. 
+
+# Dashboard
+![image](https://user-images.githubusercontent.com/84846378/236678439-c11f55f8-e41b-4f9d-89b0-ad0d673a257b.png)
+
+
+
+# Website hosted on render successfully!:- https://mihirc0111-ipl-predictor-mihir-app-8s1ct4.streamlit.app/
+
+# Dataset link:- https://www.kaggle.com/datasets/ramjidoolla/ipl-data-set?select=deliveries.csv
+
+### Continued development
+I would like to learn futher advance techniques which will help me create a beautiful UI for this website.
+
+#### Author
+
+LinkedIn - [Mihir Chavan](https://www.linkedin.com/in/mihir-chavan-643615234/)
+Github - [@mihirc0111](https://github.com/mihirc0111)
---
+++ app.py
@@ -0,0 +1,94 @@
+import streamlit as st
+import pickle
+import pandas as pd
+
+teams = [
+    'Chennai Super Kings',
+    'Delhi Capitals',
+    'Gujarat Titans',
+    'Kolkata Knight Riders',
+    'Lucknow Super Giants',
+    'Mumbai Indians',
+    'Punjab Kings',
+    'Rajasthan Royals',
+    'Royal Challengers Bangalore',
+    'Sunrisers Hyderabad'    
+]
+
+cities = ['Hyderabad', 'Bangalore', 'Mumbai', 'Indore', 'Kolkata', 'Delhi',
+       'Chandigarh', 'Jaipur', 'Chennai', 'Cape Town', 'Port Elizabeth',
+       'Durban', 'Centurion', 'East London', 'Johannesburg', 'Kimberley',
+       'Bloemfontein', 'Ahmedabad', 'Cuttack', 'Nagpur', 'Dharamsala',
+       'Visakhapatnam', 'Pune', 'Raipur', 'Ranchi', 'Abu Dhabi',
+       'Sharjah', 'Mohali', 'Bengaluru']
+
+pipe = pickle.load(open('pipe.pkl','rb'))
+<<<<<<< HEAD
+st.title('IPL-Win-Predictor-By Mihir')
+=======
+st.title('IPL-Win-Predictor')
+
+>>>>>>> 31c00a6
+
+# col1, col2 = st.beta_columns(2)
+col1, col2 = st.columns(2)
+
+with col1:
+    batting_team = st.selectbox('Select the batting team',sorted(teams))
+with col2:
+    bowling_team = st.selectbox('Select the bowling team',sorted(teams))
+
+selected_city = st.selectbox('Select host city',sorted(cities))
+
+target = st.number_input('Target')
+
+# col3,col4,col5 = st.beta_columns(3)
+col3,col4,col5 = st.columns(3)
+
+
+with col3:
+    score = st.number_input('Score')
+with col4:
+    overs = st.number_input('Overs completed')
+with col5:
+    wickets = st.number_input('Wickets out')
+
+if st.button('Predict Probability'):
+    runs_left = target - score
+    balls_left = 120 - (overs*6)
+    wickets = 10 - wickets
+    crr = score/overs
+    rrr = (runs_left*6)/balls_left
+
+    input_df = pd.DataFrame({'batting_team':[batting_team],'bowling_team':[bowling_team],'city':[selected_city],'runs_left':[runs_left],'balls_left':[balls_left],'wickets':[wickets],'total_runs_x':[target],'crr':[crr],'rrr':[rrr]})
+
+    result = pipe.predict_proba(input_df)
+    loss = result[0][0]
+    win = result[0][1]
+    st.header(batting_team + "- " + str(round(win*100)) + "%")
+    st.header(bowling_team + "- " + str(round(loss*100)) + "%")
+    
+    
+
+    
+
+st.write("")  # Add a blank line
+st.write("")  # Add a blank line
+st.write("")  # Add a blank line
+st.write("")  # Add a blank line
+# Create a container for the footer
+footer_container = st.container()
+
+# Add copyright information in the footer container
+with footer_container:
+    st.write("© 2023 Mihir Bipin Chavan. Made with ❤️ and 🐍")
+
+
+    
+    
+    
+    
+    
+    
+    
+    
